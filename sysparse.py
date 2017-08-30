4cecbfbafe8d052fd98387bf71c03ee696c24278
David Tannfors <gnupoet@gmail.com>
2017-08-30 08:54:32 +0200

    Update sysparse.py

--- sysparse.py
+++ sysparse.py
@@ -1,14 +1,13 @@
 import subprocess
 
-mem_tmp = subprocess.check_output(["cat", "/proc/meminfo"]).split("\n"); # Split every line into dict entry                                          
-del mem_tmp[-1]
-print mem_tmp
+mem_tmp = subprocess.check_output(["cat", "/proc/meminfo"]).split("\n"); # Split every line into dict entry
+del mem_tmp[-1] # Remove the last entry which is faulty for some reason
 
 i=0
-while i<len(mem_tmp):                    # loop                                                                                                      
-    mem_tmp[i]=mem_tmp[i].split(":");    # split dict into 2d.                                                                                       
-    mem_tmp[i][1]=mem_tmp[i][1].strip(); # strip white spaces from proc file.                                                                        
-    print "idx:",i,"keys:", len(mem_tmp[i]), mem_tmp[i];
+while i<len(mem_tmp):                    # loop
+    mem_tmp[i]=mem_tmp[i].split(":");    # split dict into 2d.
+    mem_tmp[i][1]=mem_tmp[i][1].strip(); # strip white spaces from proc file.
+    print "idx:",i,mem_tmp[i][0],"=>",mem_tmp[i][1];
     i=i+1
 
 
